7e1233653f8f282de91898551a331bdf7a72a6e6
Maksim Patapenka <maksim.patapenka@gmail.com>
2018-12-25 22:09:05 +0300

    Updates

--- src/main/java/org/mpatapenka/ssp/SspProperties.java
+++ src/main/java/org/mpatapenka/ssp/SspProperties.java
@@ -3,10 +3,10 @@ package org.mpatapenka.ssp;
 import com.google.common.collect.Lists;
 import lombok.Getter;
 import lombok.Setter;
+import org.mpatapenka.ssp.validation.constraint.EmailList;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -18,7 +18,7 @@ import java.util.List;
 @ConfigurationProperties("ssp")
 public class SspProperties {
     /** Email order confirmation will be send to this email list. */
-//    @Email
+    @EmailList
     private List<String> confirmationEmails = Lists.newArrayList();
 
     /** Root directory to put all loaded images. */
---
+++ src/main/java/org/mpatapenka/ssp/validation/constraint/EmailList.java
@@ -0,0 +1,27 @@
+package org.mpatapenka.ssp.validation.constraint;
+
+import org.mpatapenka.ssp.validation.validator.EmailListValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
+@Constraint(validatedBy = {EmailListValidator.class})
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EmailList {
+    String message() default "{javax.validation.constraints.Email.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
